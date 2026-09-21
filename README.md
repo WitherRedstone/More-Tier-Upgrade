414a8af26093385abf16644cd4017784dfbe8343
WitherRedstone <1751216077@qq.com>
2026-09-21 12:51:00 +0800

    1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+Add multiple new stacking levels to Sophisticated Backpacks and Sophisticated Storage..
+
+为精妙存储和精妙背包添加多个新的堆叠等级
--- src/main/java/com/chinaex123/more_tier_upgrade/data/ModDataGenerator.java
+++ src/main/java/com/chinaex123/more_tier_upgrade/data/ModDataGenerator.java
@@ -1,4 +1,4 @@
-package com.chinaex123.more_tier_upgrade.register.dataGen;
+package com.chinaex123.more_tier_upgrade.data;
 
 import com.chinaex123.more_tier_upgrade.MoreTierUpgrade;
 import net.minecraft.data.DataGenerator;
@@ -6,8 +6,6 @@ import net.minecraft.data.PackOutput;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.concurrent.CompletableFuture;
 
 @Mod.EventBusSubscriber(modid = MoreTierUpgrade.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModDataGenerator {
